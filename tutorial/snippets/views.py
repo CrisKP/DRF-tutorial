144d5c2e1fa22cefd6b5def87c6d198ab055d6fc
Cris Pope <cpope@oreilly.com>
2019-12-06 20:04:35 -0800

    Add owner to snippets and permissions + auth to the API

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,14 +1,34 @@
-from rest_framework import mixins, generics
+from django.contrib.auth.models import User
+from rest_framework import generics, permissions
 
+from snippets.permissions import IsOwnerOrReadOnly
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
+
 
 class SnippetList(generics.ListCreateAPIView):
     """List all code snippets, or create a new snippet."""
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """Retrieve, update or delete a code snippet."""
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    
+
+class UserList(generics.ListAPIView):
+    """List all the users."""
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
