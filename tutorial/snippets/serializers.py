63e84690891b78bab4f67040169d0e3d3977f8b6
Cris Pope <cpope@oreilly.com>
2019-12-16 18:25:57 -0800

    Hyperlink the API

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -4,17 +4,18 @@ from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
     
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
+        fields = ['url', 'id', 'highlight', 'title', 'code', 'linenos', 
+                  'language', 'style', 'owner']
 
-class UserSerializer(serializers.ModelSerializer):
-    # Need to explicitly add since 'snippets' is a reverse relationship on the User model
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
     
     class Meta:
         model = User
-        fields = ['id', 'username', 'snippets']
+        fields = ['url', 'id', 'username', 'snippets']
